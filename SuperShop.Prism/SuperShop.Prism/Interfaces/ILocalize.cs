74236b4060497862f380419dbbc5765fb4b0fffc
Pedro Goncalves <pedromfonsecagoncalves@gmail.com>
2023-07-21 15:54:03 +0100

    MultiLanguages

---
+++ SuperShop.Prism/SuperShop.Prism/Interfaces/ILocalize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperShop.Prism.Interfaces
+{
+    internal interface ILocalize
+    {
+        CultureInfo GetCurrentCultureINfo();
+
+        void SetLocale(CultureInfo ci);
+    }
+}
